f504b837ba71987c94ad6fb1d45eb39a9d77db28
rijusougata13 <rijusougata13.com>
2020-04-05 20:40:07 +0530

    added

---
+++ motion_detector.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  5 15:58:38 2020
+
+@author: sougata
+"""
+
+import cv2,time
+import matplotlib.pyplot as plt
+import numpy as np
+video = cv2.VideoCapture('video2.mp4')
+
+first_frame=None
+while True:
+    check,frame=video.read()
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    gray=cv2.GaussianBlur(gray,(21,21),0)
+    if first_frame is None:
+        first_frame=gray
+        continue
+    delta_frame=cv2.absdiff(first_frame,gray)
+    thresh_delta = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1] 
+    thresh_delta = cv2.dilate(thresh_delta, None, iterations = 0) 
+    (cnts,_ )= cv2.findContours(thresh_delta.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    for contour in cnts: 
+        if cv2.contourArea(contour) < 1000: 
+            continue
+        (x, y, w, h) = cv2.boundingRect(contour) 
+        # making green rectangle arround the moving object 
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3) 
+    cv2.imshow('frame',frame)
+#    cv2.imshow('thres',thresh_delta)
+    key=cv2.waitKey(12)
+    if key==ord('q'):
+        break
+video.release()
+cv2.destroyAllWindows()
+        
